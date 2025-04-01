8d5f7fd83bedf85ab851815100eed69105cfd03d
CharliePatel <charliepatel@kubrickgroup.com>
2025-04-01 14:58:22 +0100

    Logger configerations for project

--- logger_config.py
+++ logger_config.py
@@ -4,9 +4,13 @@ def setup_logging():
     logger = logging.getLogger("currency_converter")
     logger.setLevel(logging.INFO)
 
+# Set up handlers if they dont exist
     if not logger.handlers:
-        fh = logging.FileHandler("converter.log")
+        fh = logging.FileHandler("converter.log", mode = "w")
+        fh.setLevel("INFO")
+
         sh = logging.StreamHandler()
+        sh.setLevel("INFO")
 
         formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
         fh.setFormatter(formatter)
